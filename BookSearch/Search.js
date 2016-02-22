35d4cf252e6048d93c8d6508be9f975fde5db3f1
Andrew Dinh <andrewdinh74@yahoo.com>
2016-02-22 10:47:19 -0800

    Modify Search.js

--- BookSearch/Search.js
+++ BookSearch/Search.js
@@ -1,36 +1,67 @@
+// 'use strict';
+//
+// var React = require('react-native');
+//
+// var {
+//   StyleSheet,
+//   View,
+//   Text,
+//   Component
+// } = React;
+//
+// var styles = StyleSheet.create({
+//   description: {
+//     fontSize: 20,
+//     backgroundColor: 'white'
+//   },
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center'
+//   }
+// });
+//
+// class Search extends Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text style={styles.description}>
+//           Search Tab
+//         </Text>
+//       </View>
+//     );
+//   }
+// }
+//
+// module.exports = Search;
 'use strict';
 
 var React = require('react-native');
+var SearchBooks = require('./SearchBooks');
 
 var {
-  StyleSheet,
-  View,
-  Text,
-  Component
-} = React;
+    StyleSheet,
+    NavigatorIOS,
+    Component
+   } = React;
 
 var styles = StyleSheet.create({
-  description: {
-    fontSize: 20,
-    backgroundColor: 'white'
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+    container: {
+        flex: 1
+    }
 });
 
 class Search extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.description}>
-          Search Tab
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <NavigatorIOS
+                style={styles.container}
+                initialRoute={{
+            title: 'Search Books',
+            component: SearchBooks
+        }}/>
+        );
+    }
 }
 
 module.exports = Search;
