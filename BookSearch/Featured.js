aeae4b3fe74387bf2faae593248f2cd36af92943
Andrew Dinh <andrewdinh74@yahoo.com>
2016-02-22 10:45:21 -0800

    Modify Feature.js

--- BookSearch/Featured.js
+++ BookSearch/Featured.js
@@ -1,36 +1,67 @@
+// 'use strict';
+//
+// var React = require('react-native');
+//
+// var {
+//   StyleSheet,
+//   View,
+//   Text,
+//   Component
+// } = React;
+//
+// var styles = StyleSheet.create({
+//   description: {
+//     fontSize: 20,
+//     backgroundColor: 'red'
+//   },
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center'
+//   }
+// });
+//
+// class Featured extends Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text style={styles.description}>
+//           Featured Tab
+//         </Text>
+//       </View>
+//     );
+//   }
+// }
+//
+// module.exports = Featured;
 'use strict';
 
 var React = require('react-native');
+var BookList = require('./BookList');
 
 var {
-  StyleSheet,
-  View,
-  Text,
-  Component
-} = React;
+    StyleSheet,
+    NavigatorIOS,
+    Component
+   } = React;
 
 var styles = StyleSheet.create({
-  description: {
-    fontSize: 20,
-    backgroundColor: 'red'
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+    container: {
+        flex: 1
+    }
 });
 
 class Featured extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.description}>
-          Featured Tab
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <NavigatorIOS
+                style={styles.container}
+                initialRoute={{
+            title: 'Featured Books',
+            component: BookList
+            }}/>
+        );
+    }
 }
 
 module.exports = Featured;
