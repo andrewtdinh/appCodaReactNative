479ed2fb5db9fe15b289039bab7ecce3e3aa2783
Andrew Dinh <andrewdinh74@yahoo.com>
2016-02-23 10:23:59 -0800

    Refactor Feature.js format

--- BookSearch/Featured.js
+++ BookSearch/Featured.js
@@ -40,28 +40,28 @@ var React = require('react-native');
 var BookList = require('./BookList');
 
 var {
-    StyleSheet,
-    NavigatorIOS,
-    Component
-   } = React;
+  StyleSheet,
+  NavigatorIOS,
+  Component
+} = React;
 
 var styles = StyleSheet.create({
-    container: {
-        flex: 1
-    }
+  container: {
+    flex: 1
+  }
 });
 
 class Featured extends Component {
-    render() {
-        return (
-            <NavigatorIOS
-                style={styles.container}
-                initialRoute={{
-            title: 'Featured Books',
-            component: BookList
-            }}/>
-        );
+  render() {
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Featured Books',
+          component: BookList
+        }}/>
+      );
     }
-}
+  }
 
-module.exports = Featured;
+  module.exports = Featured;
