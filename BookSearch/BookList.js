08a27c3c489087c16e7560d5b85aea49e6bbb0c0
Andrew Dinh <andrewdinh74@yahoo.com>
2016-02-23 10:21:57 -0800

    Add fake book data to BookList.js

--- BookSearch/BookList.js
+++ BookSearch/BookList.js
@@ -12,6 +12,10 @@ var styles = StyleSheet.create({
 
 });
 
+var FAKE_BOOK_DATA = [
+    {volumeInfo: {title: 'The Catcher in the Rye', authors: "J. D. Salinger", imageLinks: {thumbnail: 'http://books.google.com/books/content?id=PCDengEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api'}}}
+];
+
 class BookList extends Component {
     render() {
         return (
