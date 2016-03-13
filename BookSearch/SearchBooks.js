6e3479f7171fa08aaab9d3a37bd93361557fc9ca
Andrew Dinh <andrewdinh74@yahoo.com>
2016-03-12 21:30:58 -0800

    Reformate the modified section

--- BookSearch/SearchBooks.js
+++ BookSearch/SearchBooks.js
@@ -3,57 +3,57 @@
 var React = require('react-native');
 var SearchResults = require('./SearchResults');
 var {
-    StyleSheet,
-    View,
-    Text,
-    Component,
-    TextInput,
-    TouchableHighlight,
-    ActivityIndicatorIOS
-    } = React;
+  StyleSheet,
+  View,
+  Text,
+  Component,
+  TextInput,
+  TouchableHighlight,
+  ActivityIndicatorIOS
+} = React;
 
 var styles = StyleSheet.create({
-    container: {
-        marginTop: 65,
-        padding: 10
-    },
-    searchInput: {
-        height: 36,
-        marginTop: 10,
-        marginBottom: 10,
-        fontSize: 18,
-        borderWidth: 1,
-        flex: 1,
-        borderRadius: 4,
-        padding: 5
-    },
-    button: {
-        height: 36,
-        backgroundColor: '#f39c12',
-        borderRadius: 8,
-        justifyContent: 'center',
-        marginTop: 15
-    },
-    buttonText: {
-        fontSize: 18,
-        color: 'white',
-        alignSelf: 'center'
-    },
-    instructions: {
-        fontSize: 18,
-        alignSelf: 'center',
-        marginBottom: 15
-    },
-    fieldLabel: {
-        fontSize: 15,
-        marginTop: 15
-    },
-    errorMessage: {
-        fontSize: 15,
-        alignSelf: 'center',
-        marginTop: 15,
-        color: 'red'
-    }
+  container: {
+    marginTop: 65,
+    padding: 10
+  },
+  searchInput: {
+    height: 36,
+    marginTop: 10,
+    marginBottom: 10,
+    fontSize: 18,
+    borderWidth: 1,
+    flex: 1,
+    borderRadius: 4,
+    padding: 5
+  },
+  button: {
+    height: 36,
+    backgroundColor: '#f39c12',
+    borderRadius: 8,
+    justifyContent: 'center',
+    marginTop: 15
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  instructions: {
+    fontSize: 18,
+    alignSelf: 'center',
+    marginBottom: 15
+  },
+  fieldLabel: {
+    fontSize: 15,
+    marginTop: 15
+  },
+  errorMessage: {
+    fontSize: 15,
+    alignSelf: 'center',
+    marginTop: 15,
+    color: 'red'
+  }
 });
 
 class SearchBooks extends Component {
