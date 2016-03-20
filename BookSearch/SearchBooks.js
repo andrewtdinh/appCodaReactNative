b835ab1fdfb70afc412fefc8184b1caed492cd94
Andrew Dinh <andrewdinh74@yahoo.com>
2016-03-19 18:39:09 -0700

    Reformat SearchBook.js

--- BookSearch/SearchBooks.js
+++ BookSearch/SearchBooks.js
@@ -74,77 +74,77 @@ class SearchBooks extends Component {
     ( <ActivityIndicatorIOS
       hidden='true'
       size='large'/> ) :
-      ( <View/>);
-      return (
-        <View style={styles.container}>
-          <Text style={styles.instructions}>Search by book title and/or author</Text>
-          <View>
-            <Text style={styles.fieldLabel}>Book Title:</Text>
-            <TextInput style={styles.searchInput} onChange={this.bookTitleInput.bind(this)}/>
-          </View>
-          <View>
-            <Text style={styles.fieldLabel}>Author:</Text>
-            <TextInput style={styles.searchInput} onChange={this.bookAuthorInput.bind(this)}/>
-          </View>
-          <TouchableHighlight style={styles.button}
-            underlayColor='#f1c40f'
-            onPress={this.searchBooks.bind(this)}>
-            <Text style={styles.buttonText}>Search</Text>
-          </TouchableHighlight>
-          {spinner}
-          <Text style={styles.errorMessage}>{this.state.errorMessage}</Text>
+    ( <View/>);
+    return (
+      <View style={styles.container}>
+        <Text style={styles.instructions}>Search by book title and/or author</Text>
+        <View>
+          <Text style={styles.fieldLabel}>Book Title:</Text>
+          <TextInput style={styles.searchInput} onChange={this.bookTitleInput.bind(this)}/>
         </View>
-      );
+        <View>
+          <Text style={styles.fieldLabel}>Author:</Text>
+          <TextInput style={styles.searchInput} onChange={this.bookAuthorInput.bind(this)}/>
+        </View>
+        <TouchableHighlight style={styles.button}
+          underlayColor='#f1c40f'
+          onPress={this.searchBooks.bind(this)}>
+          <Text style={styles.buttonText}>Search</Text>
+        </TouchableHighlight>
+        {spinner}
+        <Text style={styles.errorMessage}>{this.state.errorMessage}</Text>
+      </View>
+    );
+  }
+
+  bookTitleInput(event) {
+    this.setState({ bookTitle: event.nativeEvent.text });
+  }
+
+  bookAuthorInput(event) {
+    this.setState({ bookAuthor: event.nativeEvent.text });
+  }
+
+  searchBooks() {
+    this.fetchData();
+  }
+
+  fetchData() {
+
+    this.setState({ isLoading: true });
+
+    var baseURL = 'https://www.googleapis.com/books/v1/volumes?q=';
+    if (this.state.bookAuthor !== '') {
+      baseURL += encodeURIComponent('inauthor:' + this.state.bookAuthor);
+    }
+    if (this.state.bookTitle !== '') {
+      baseURL += (this.state.bookAuthor === '') ? encodeURIComponent('intitle:' + this.state.bookTitle) : encodeURIComponent('+intitle:' + this.state.bookTitle);
     }
 
-    bookTitleInput(event) {
-      this.setState({ bookTitle: event.nativeEvent.text });
-    }
+    console.log('URL: >>> ' + baseURL);
+    fetch(baseURL)
+    .then((response) => response.json())
+    .then((responseData) => {
+      this.setState({ isLoading: false});
+      if (responseData.items) {
 
-    bookAuthorInput(event) {
-      this.setState({ bookAuthor: event.nativeEvent.text });
-    }
-
-    searchBooks() {
-      this.fetchData();
-    }
-
-    fetchData() {
-
-      this.setState({ isLoading: true });
-
-      var baseURL = 'https://www.googleapis.com/books/v1/volumes?q=';
-      if (this.state.bookAuthor !== '') {
-        baseURL += encodeURIComponent('inauthor:' + this.state.bookAuthor);
+        this.props.navigator.push({
+          title: 'Search Results',
+          component: SearchResults,
+          passProps: {books: responseData.items}
+        });
+      } else {
+        this.setState({ errorMessage: 'No results found'});
       }
-      if (this.state.bookTitle !== '') {
-        baseURL += (this.state.bookAuthor === '') ? encodeURIComponent('intitle:' + this.state.bookTitle) : encodeURIComponent('+intitle:' + this.state.bookTitle);
-      }
-
-      console.log('URL: >>> ' + baseURL);
-      fetch(baseURL)
-      .then((response) => response.json())
-      .then((responseData) => {
-        this.setState({ isLoading: false});
-        if (responseData.items) {
-
-          this.props.navigator.push({
-            title: 'Search Results',
-            component: SearchResults,
-            passProps: {books: responseData.items}
-          });
-        } else {
-          this.setState({ errorMessage: 'No results found'});
-        }
-      })
-      .catch(error =>
-        this.setState({
-          isLoading: false,
-          errorMessage: error
-        }))
-        .done();
-      }
-
+    })
+    .catch(error =>
+      this.setState({
+        isLoading: false,
+        errorMessage: error
+      }))
+      .done();
     }
 
-    module.exports = SearchBooks;
+  }
+
+  module.exports = SearchBooks;
